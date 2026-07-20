047878b011ca5e558fcb0827a59520d11ee3a6fb
yimgruui <yimgruui@foxmail.com>
2026-07-20 08:52:40 +0800

    Update diy-rax3000m.sh

--- sh/diy-rax3000m.sh
+++ sh/diy-rax3000m.sh
@@ -7,7 +7,7 @@ sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
 sed -i '/CONFIG_BUILDBOT/d' include/feeds.mk
 sed -i 's/;)\s*\\/; \\/' include/feeds.mk
 
-# 修改openwrt登陆地址,把下面的192.168.11.1修改成你想要的就可以了
+# 修改openwrt登陆地址
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 
